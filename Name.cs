3ad2e013e2b9ae7ec3348741d2444a0be9490818
MargaretDemetriou <31332493+MargaretDemetriou@users.noreply.github.com>
2018-02-07 14:38:05 +1000

    Create Name.cs

---
+++ Name.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name_sorter
+{
+    public class Name
+    {
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+        public string thirdName { get; set; }
+        public string lastName { get; set; }
+    }
+}
